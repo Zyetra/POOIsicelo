49213ca9228d76646d2bd5fa9fb87b27722f8f25
Zyetra <93579650+Zyetra@users.noreply.github.com>
2021-11-26 11:19:16 +0100

    .h gcl

---
+++ POOIsicelo/GestionClient.h
@@ -0,0 +1,84 @@
+#pragma once
+
+namespace POOIsicelo {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Description résumée de Form1
+	/// </summary>
+	public ref class Form1 : public System::Windows::Forms::Form
+	{
+	public:
+		Form1(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: ajoutez ici le code du constructeur
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Nettoyage des ressources utilisées.
+		/// </summary>
+		~Form1()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^ buttonRetour;
+	protected:
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Variable nécessaire au concepteur.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+		/// le contenu de cette méthode avec l'éditeur de code.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->buttonRetour = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// buttonRetour
+			// 
+			this->buttonRetour->Location = System::Drawing::Point(86, 103);
+			this->buttonRetour->Name = L"buttonRetour";
+			this->buttonRetour->Size = System::Drawing::Size(102, 48);
+			this->buttonRetour->TabIndex = 0;
+			this->buttonRetour->Text = L"Retour";
+			this->buttonRetour->UseVisualStyleBackColor = true;
+			this->buttonRetour->Click += gcnew System::EventHandler(this, &Form1::buttonRetour_Click);
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(282, 253);
+			this->Controls->Add(this->buttonRetour);
+			this->Name = L"Form1";
+			this->Text = L"Form1";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void buttonRetour_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->Close();
+	}
+	};
+}
---
+++ POOIsicelo/Isicelo.h
@@ -0,0 +1,86 @@
+#pragma once
+#include "GestionClient.h"
+
+namespace POOIsicelo {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Description résumée de MyForm
+	/// </summary>
+	public ref class Form : public System::Windows::Forms::Form
+	{
+	public:
+		Form(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: ajoutez ici le code du constructeur
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Nettoyage des ressources utilisées.
+		/// </summary>
+		~Form()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^ button1;
+	protected:
+
+	private:
+		/// <summary>
+		/// Variable nécessaire au concepteur.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+		/// le contenu de cette méthode avec l'éditeur de code.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(59, 116);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(152, 47);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Gestion Client";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Form::button1_Click);
+			// 
+			// Form
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(282, 253);
+			this->Controls->Add(this->button1);
+			this->Name = L"Form";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->Hide();
+		Form1^ APP = gcnew Form1();
+		APP->ShowDialog();
+		this->Show();
+	}
+	};
+}
