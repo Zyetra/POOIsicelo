7c6f8fbec7ea450d18f6a5f88be01cee857f17ae
Zyetra <93579650+Zyetra@users.noreply.github.com>
2021-11-30 14:13:22 +0100

    Modif Gestion Client

--- POOIsicelo/GestionClient.h
+++ POOIsicelo/GestionClient.h
@@ -74,15 +74,15 @@ namespace POOIsicelo {
 
 
 
-	private: System::Windows::Forms::TextBox^ codePostalFacturation;
 
-	private: System::Windows::Forms::Label^ label16;
-	private: System::Windows::Forms::ComboBox^ villeFacturation;
-	private: System::Windows::Forms::Label^ label17;
-	private: System::Windows::Forms::TextBox^ rueFacturation;
-	private: System::Windows::Forms::Label^ label18;
-	private: System::Windows::Forms::TextBox^ numFacturation;
-	private: System::Windows::Forms::Label^ label19;
+
+
+
+
+
+
+
+
 	private: System::Windows::Forms::GroupBox^ infoClient;
 
 
@@ -116,18 +116,18 @@ namespace POOIsicelo {
 	private: System::Windows::Forms::Label^ label10;
 	private: System::Windows::Forms::Label^ label11;
 	private: System::Windows::Forms::TextBox^ rueAdresse;
-	private: System::Windows::Forms::GroupBox^ adresseFacturation;
-	private: System::Windows::Forms::GroupBox^ adresseLivraison;
 
 
-	private: System::Windows::Forms::Label^ label1;
-	private: System::Windows::Forms::TextBox^ textBox4;
-	private: System::Windows::Forms::Label^ label3;
-	private: System::Windows::Forms::TextBox^ textBox5;
-	private: System::Windows::Forms::Label^ label4;
-	private: System::Windows::Forms::ComboBox^ comboBox1;
-	private: System::Windows::Forms::Label^ label5;
-	private: System::Windows::Forms::TextBox^ textBox6;
+
+
+
+
+
+
+
+
+
+
 private: System::Windows::Forms::Button^ affListeClient;
 
 
@@ -159,14 +159,6 @@ private: System::Windows::Forms::Button^ affListeClient;
 			this->label8 = (gcnew System::Windows::Forms::Label());
 			this->label13 = (gcnew System::Windows::Forms::Label());
 			this->numClient = (gcnew System::Windows::Forms::TextBox());
-			this->codePostalFacturation = (gcnew System::Windows::Forms::TextBox());
-			this->label16 = (gcnew System::Windows::Forms::Label());
-			this->villeFacturation = (gcnew System::Windows::Forms::ComboBox());
-			this->label17 = (gcnew System::Windows::Forms::Label());
-			this->rueFacturation = (gcnew System::Windows::Forms::TextBox());
-			this->label18 = (gcnew System::Windows::Forms::Label());
-			this->numFacturation = (gcnew System::Windows::Forms::TextBox());
-			this->label19 = (gcnew System::Windows::Forms::Label());
 			this->infoClient = (gcnew System::Windows::Forms::GroupBox());
 			this->label14 = (gcnew System::Windows::Forms::Label());
 			this->label15 = (gcnew System::Windows::Forms::Label());
@@ -183,22 +175,10 @@ private: System::Windows::Forms::Button^ affListeClient;
 			this->label10 = (gcnew System::Windows::Forms::Label());
 			this->label11 = (gcnew System::Windows::Forms::Label());
 			this->rueAdresse = (gcnew System::Windows::Forms::TextBox());
-			this->adresseFacturation = (gcnew System::Windows::Forms::GroupBox());
-			this->adresseLivraison = (gcnew System::Windows::Forms::GroupBox());
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
-			this->label3 = (gcnew System::Windows::Forms::Label());
-			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
-			this->label4 = (gcnew System::Windows::Forms::Label());
-			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
-			this->label5 = (gcnew System::Windows::Forms::Label());
-			this->textBox6 = (gcnew System::Windows::Forms::TextBox());
 			this->affListeClient = (gcnew System::Windows::Forms::Button());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			this->infoClient->SuspendLayout();
 			this->adresseDomicile->SuspendLayout();
-			this->adresseFacturation->SuspendLayout();
-			this->adresseLivraison->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// buttonRetour
@@ -280,7 +260,7 @@ private: System::Windows::Forms::Button^ affListeClient;
 			this->datePremierAchat->Location = System::Drawing::Point(7, 218);
 			this->datePremierAchat->Margin = System::Windows::Forms::Padding(4);
 			this->datePremierAchat->Name = L"datePremierAchat";
-			this->datePremierAchat->Size = System::Drawing::Size(132, 22);
+			this->datePremierAchat->Size = System::Drawing::Size(236, 22);
 			this->datePremierAchat->TabIndex = 16;
 			// 
 			// label8
@@ -309,81 +289,8 @@ private: System::Windows::Forms::Button^ affListeClient;
 			this->numClient->Location = System::Drawing::Point(87, 246);
 			this->numClient->Margin = System::Windows::Forms::Padding(4);
 			this->numClient->Name = L"numClient";
-			this->numClient->Size = System::Drawing::Size(52, 22);
+			this->numClient->Size = System::Drawing::Size(86, 22);
 			this->numClient->TabIndex = 27;
-			// 
-			// codePostalFacturation
-			// 
-			this->codePostalFacturation->Location = System::Drawing::Point(7, 216);
-			this->codePostalFacturation->Margin = System::Windows::Forms::Padding(4);
-			this->codePostalFacturation->Name = L"codePostalFacturation";
-			this->codePostalFacturation->Size = System::Drawing::Size(132, 22);
-			this->codePostalFacturation->TabIndex = 37;
-			// 
-			// label16
-			// 
-			this->label16->AutoSize = true;
-			this->label16->Location = System::Drawing::Point(12, 196);
-			this->label16->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
-			this->label16->Name = L"label16";
-			this->label16->Size = System::Drawing::Size(89, 16);
-			this->label16->TabIndex = 36;
-			this->label16->Text = L" Code postal :";
-			// 
-			// villeFacturation
-			// 
-			this->villeFacturation->FormattingEnabled = true;
-			this->villeFacturation->Location = System::Drawing::Point(7, 158);
-			this->villeFacturation->Margin = System::Windows::Forms::Padding(4);
-			this->villeFacturation->Name = L"villeFacturation";
-			this->villeFacturation->Size = System::Drawing::Size(160, 24);
-			this->villeFacturation->TabIndex = 35;
-			// 
-			// label17
-			// 
-			this->label17->AutoSize = true;
-			this->label17->Location = System::Drawing::Point(15, 134);
-			this->label17->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
-			this->label17->Name = L"label17";
-			this->label17->Size = System::Drawing::Size(39, 16);
-			this->label17->TabIndex = 34;
-			this->label17->Text = L"Ville :";
-			// 
-			// rueFacturation
-			// 
-			this->rueFacturation->Location = System::Drawing::Point(7, 100);
-			this->rueFacturation->Margin = System::Windows::Forms::Padding(4);
-			this->rueFacturation->Name = L"rueFacturation";
-			this->rueFacturation->Size = System::Drawing::Size(132, 22);
-			this->rueFacturation->TabIndex = 33;
-			// 
-			// label18
-			// 
-			this->label18->AutoSize = true;
-			this->label18->Location = System::Drawing::Point(15, 80);
-			this->label18->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
-			this->label18->Name = L"label18";
-			this->label18->Size = System::Drawing::Size(38, 16);
-			this->label18->TabIndex = 32;
-			this->label18->Text = L"Rue :";
-			// 
-			// numFacturation
-			// 
-			this->numFacturation->Location = System::Drawing::Point(7, 44);
-			this->numFacturation->Margin = System::Windows::Forms::Padding(4);
-			this->numFacturation->Name = L"numFacturation";
-			this->numFacturation->Size = System::Drawing::Size(132, 22);
-			this->numFacturation->TabIndex = 31;
-			// 
-			// label19
-			// 
-			this->label19->AutoSize = true;
-			this->label19->Location = System::Drawing::Point(15, 26);
-			this->label19->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
-			this->label19->Name = L"label19";
-			this->label19->Size = System::Drawing::Size(61, 16);
-			this->label19->TabIndex = 30;
-			this->label19->Text = L"Numéro :";
 			// 
 			// infoClient
 			// 
@@ -397,9 +304,9 @@ private: System::Windows::Forms::Button^ affListeClient;
 			this->infoClient->Controls->Add(this->datePremierAchat);
 			this->infoClient->Controls->Add(this->label13);
 			this->infoClient->Controls->Add(this->numClient);
-			this->infoClient->Location = System::Drawing::Point(200, 15);
+			this->infoClient->Location = System::Drawing::Point(280, 15);
 			this->infoClient->Name = L"infoClient";
-			this->infoClient->Size = System::Drawing::Size(185, 274);
+			this->infoClient->Size = System::Drawing::Size(250, 274);
 			this->infoClient->TabIndex = 48;
 			this->infoClient->TabStop = false;
 			this->infoClient->Text = L"Informations Client";
@@ -429,7 +336,7 @@ private: System::Windows::Forms::Button^ affListeClient;
 			this->dateNaissanceClient->Location = System::Drawing::Point(7, 163);
 			this->dateNaissanceClient->Margin = System::Windows::Forms::Padding(4);
 			this->dateNaissanceClient->Name = L"dateNaissanceClient";
-			this->dateNaissanceClient->Size = System::Drawing::Size(132, 22);
+			this->dateNaissanceClient->Size = System::Drawing::Size(236, 22);
 			this->dateNaissanceClient->TabIndex = 20;
 			// 
 			// nomClient
@@ -437,7 +344,7 @@ private: System::Windows::Forms::Button^ affListeClient;
 			this->nomClient->Location = System::Drawing::Point(7, 49);
 			this->nomClient->Margin = System::Windows::Forms::Padding(4);
 			this->nomClient->Name = L"nomClient";
-			this->nomClient->Size = System::Drawing::Size(132, 22);
+			this->nomClient->Size = System::Drawing::Size(236, 22);
 			this->nomClient->TabIndex = 16;
 			// 
 			// label20
@@ -455,7 +362,7 @@ private: System::Windows::Forms::Button^ affListeClient;
 			this->prenomClient->Location = System::Drawing::Point(7, 105);
 			this->prenomClient->Margin = System::Windows::Forms::Padding(4);
 			this->prenomClient->Name = L"prenomClient";
-			this->prenomClient->Size = System::Drawing::Size(132, 22);
+			this->prenomClient->Size = System::Drawing::Size(236, 22);
 			this->prenomClient->TabIndex = 18;
 			// 
 			// adresseDomicile
@@ -468,9 +375,9 @@ private: System::Windows::Forms::Button^ affListeClient;
 			this->adresseDomicile->Controls->Add(this->label10);
 			this->adresseDomicile->Controls->Add(this->label11);
 			this->adresseDomicile->Controls->Add(this->rueAdresse);
-			this->adresseDomicile->Location = System::Drawing::Point(400, 15);
+			this->adresseDomicile->Location = System::Drawing::Point(644, 15);
 			this->adresseDomicile->Name = L"adresseDomicile";
-			this->adresseDomicile->Size = System::Drawing::Size(185, 274);
+			this->adresseDomicile->Size = System::Drawing::Size(250, 274);
 			this->adresseDomicile->TabIndex = 49;
 			this->adresseDomicile->TabStop = false;
 			this->adresseDomicile->Text = L"Adresse Domicile";
@@ -480,7 +387,7 @@ private: System::Windows::Forms::Button^ affListeClient;
 			this->codePostalDomicile->Location = System::Drawing::Point(7, 221);
 			this->codePostalDomicile->Margin = System::Windows::Forms::Padding(4);
 			this->codePostalDomicile->Name = L"codePostalDomicile";
-			this->codePostalDomicile->Size = System::Drawing::Size(132, 22);
+			this->codePostalDomicile->Size = System::Drawing::Size(236, 22);
 			this->codePostalDomicile->TabIndex = 33;
 			// 
 			// label9
@@ -508,7 +415,7 @@ private: System::Windows::Forms::Button^ affListeClient;
 			this->numAdresse->Location = System::Drawing::Point(7, 49);
 			this->numAdresse->Margin = System::Windows::Forms::Padding(4);
 			this->numAdresse->Name = L"numAdresse";
-			this->numAdresse->Size = System::Drawing::Size(132, 22);
+			this->numAdresse->Size = System::Drawing::Size(236, 22);
 			this->numAdresse->TabIndex = 27;
 			// 
 			// villeAdresse
@@ -517,7 +424,7 @@ private: System::Windows::Forms::Button^ affListeClient;
 			this->villeAdresse->Location = System::Drawing::Point(7, 163);
 			this->villeAdresse->Margin = System::Windows::Forms::Padding(4);
 			this->villeAdresse->Name = L"villeAdresse";
-			this->villeAdresse->Size = System::Drawing::Size(160, 24);
+			this->villeAdresse->Size = System::Drawing::Size(236, 24);
 			this->villeAdresse->TabIndex = 31;
 			// 
 			// label10
@@ -545,115 +452,8 @@ private: System::Windows::Forms::Button^ affListeClient;
 			this->rueAdresse->Location = System::Drawing::Point(7, 105);
 			this->rueAdresse->Margin = System::Windows::Forms::Padding(4);
 			this->rueAdresse->Name = L"rueAdresse";
-			this->rueAdresse->Size = System::Drawing::Size(132, 22);
+			this->rueAdresse->Size = System::Drawing::Size(236, 22);
 			this->rueAdresse->TabIndex = 29;
-			// 
-			// adresseFacturation
-			// 
-			this->adresseFacturation->Controls->Add(this->label19);
-			this->adresseFacturation->Controls->Add(this->numFacturation);
-			this->adresseFacturation->Controls->Add(this->label18);
-			this->adresseFacturation->Controls->Add(this->rueFacturation);
-			this->adresseFacturation->Controls->Add(this->label17);
-			this->adresseFacturation->Controls->Add(this->villeFacturation);
-			this->adresseFacturation->Controls->Add(this->label16);
-			this->adresseFacturation->Controls->Add(this->codePostalFacturation);
-			this->adresseFacturation->Location = System::Drawing::Point(599, 15);
-			this->adresseFacturation->Name = L"adresseFacturation";
-			this->adresseFacturation->Size = System::Drawing::Size(185, 274);
-			this->adresseFacturation->TabIndex = 50;
-			this->adresseFacturation->TabStop = false;
-			this->adresseFacturation->Text = L"Adresse Facturation";
-			// 
-			// adresseLivraison
-			// 
-			this->adresseLivraison->Controls->Add(this->label1);
-			this->adresseLivraison->Controls->Add(this->textBox4);
-			this->adresseLivraison->Controls->Add(this->label3);
-			this->adresseLivraison->Controls->Add(this->textBox5);
-			this->adresseLivraison->Controls->Add(this->label4);
-			this->adresseLivraison->Controls->Add(this->comboBox1);
-			this->adresseLivraison->Controls->Add(this->label5);
-			this->adresseLivraison->Controls->Add(this->textBox6);
-			this->adresseLivraison->Location = System::Drawing::Point(798, 15);
-			this->adresseLivraison->Name = L"adresseLivraison";
-			this->adresseLivraison->Size = System::Drawing::Size(185, 274);
-			this->adresseLivraison->TabIndex = 51;
-			this->adresseLivraison->TabStop = false;
-			this->adresseLivraison->Text = L"Adresse Livraison";
-			// 
-			// label1
-			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(15, 26);
-			this->label1->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(61, 16);
-			this->label1->TabIndex = 30;
-			this->label1->Text = L"Numéro :";
-			// 
-			// textBox4
-			// 
-			this->textBox4->Location = System::Drawing::Point(7, 44);
-			this->textBox4->Margin = System::Windows::Forms::Padding(4);
-			this->textBox4->Name = L"textBox4";
-			this->textBox4->Size = System::Drawing::Size(132, 22);
-			this->textBox4->TabIndex = 31;
-			// 
-			// label3
-			// 
-			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(15, 80);
-			this->label3->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
-			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(38, 16);
-			this->label3->TabIndex = 32;
-			this->label3->Text = L"Rue :";
-			// 
-			// textBox5
-			// 
-			this->textBox5->Location = System::Drawing::Point(7, 100);
-			this->textBox5->Margin = System::Windows::Forms::Padding(4);
-			this->textBox5->Name = L"textBox5";
-			this->textBox5->Size = System::Drawing::Size(132, 22);
-			this->textBox5->TabIndex = 33;
-			// 
-			// label4
-			// 
-			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(15, 134);
-			this->label4->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
-			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(39, 16);
-			this->label4->TabIndex = 34;
-			this->label4->Text = L"Ville :";
-			// 
-			// comboBox1
-			// 
-			this->comboBox1->FormattingEnabled = true;
-			this->comboBox1->Location = System::Drawing::Point(7, 158);
-			this->comboBox1->Margin = System::Windows::Forms::Padding(4);
-			this->comboBox1->Name = L"comboBox1";
-			this->comboBox1->Size = System::Drawing::Size(160, 24);
-			this->comboBox1->TabIndex = 35;
-			// 
-			// label5
-			// 
-			this->label5->AutoSize = true;
-			this->label5->Location = System::Drawing::Point(12, 196);
-			this->label5->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
-			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(89, 16);
-			this->label5->TabIndex = 36;
-			this->label5->Text = L" Code postal :";
-			// 
-			// textBox6
-			// 
-			this->textBox6->Location = System::Drawing::Point(7, 216);
-			this->textBox6->Margin = System::Windows::Forms::Padding(4);
-			this->textBox6->Name = L"textBox6";
-			this->textBox6->Size = System::Drawing::Size(132, 22);
-			this->textBox6->TabIndex = 37;
 			// 
 			// affListeClient
 			// 
@@ -671,8 +471,6 @@ private: System::Windows::Forms::Button^ affListeClient;
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(995, 533);
 			this->Controls->Add(this->affListeClient);
-			this->Controls->Add(this->adresseLivraison);
-			this->Controls->Add(this->adresseFacturation);
 			this->Controls->Add(this->adresseDomicile);
 			this->Controls->Add(this->infoClient);
 			this->Controls->Add(this->dataGridView1);
@@ -690,10 +488,6 @@ private: System::Windows::Forms::Button^ affListeClient;
 			this->infoClient->PerformLayout();
 			this->adresseDomicile->ResumeLayout(false);
 			this->adresseDomicile->PerformLayout();
-			this->adresseFacturation->ResumeLayout(false);
-			this->adresseFacturation->PerformLayout();
-			this->adresseLivraison->ResumeLayout(false);
-			this->adresseLivraison->PerformLayout();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
